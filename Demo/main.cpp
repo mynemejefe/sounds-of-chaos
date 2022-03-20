15aa4e11d1b4cfd03327c01687f145c3e31ceb94
Liviusz Szalma <66947024+mynemejefe@users.noreply.github.com>
2022-03-20 14:54:06 +0100

    Initial commit

---
+++ Demo/main.cpp
@@ -0,0 +1,48 @@
+#include <Dragonfly/editor.h>		 //inlcludes most features
+#include <Dragonfly/detail/buffer.h> //will be replaced
+#include <Dragonfly/detail/vao.h>	 //will be replaced
+
+int main(int argc, char* args[])
+{
+	df::Sample sam("Dragonfly Demo", 720, 480, df::Sample::FLAGS::DEFAULT);
+	// df::Sample simplifies OpenGL, SDL, ImGui, RenderDoc in the render loop, and handles user input via callback member functions in priority queues
+	df::Camera cam;								// Implements a camera event class with handles
+	sam.AddHandlerClass(cam, 5);				// class callbacks will be called to change its state
+	sam.AddHandlerClass<df::ImGuiHandler>(10);	// static handle functions only
+
+	eltecg::ogl::ArrayBuffer MyVBO;	MyVBO.constructMutable(std::vector<glm::vec2>{ {-1, -1}, { 1, -1 }, { 0, 1 }}, GL_STATIC_DRAW);
+	eltecg::ogl::VertexArray MyVAO;	MyVAO.addVBO<glm::vec2>(MyVBO);		//these two classes will be removed from Dragonfly as soon as we have the replacement ready
+
+	df::VaoArrays demoVao((GLuint)MyVAO, GL_TRIANGLE_STRIP, 3, 0u); // temporary solution that wraps an ID
+
+	df::TextureCube<> testCubemap("Assets/xpos.png", "Assets/xneg.png", "Assets/ypos.png", "Assets/yneg.png", "Assets/zpos.png", "Assets/zneg.png");
+	df::Texture2D<> testTex = testCubemap[df::X_POS]; // 2D view of a cubemap face
+
+	df::ShaderProgramEditorVF program = "MyShaderProgram";
+	program << "Shaders/vert.vert"_vert << "Shaders/frag.frag"_frag << df::LinkProgram;
+	df::ShaderProgramEditorVF postprocess = "Postprocess shader program";
+	postprocess << "Shaders/postprocess.vert"_vert << "Shaders/postprocess.frag"_frag << df::LinkProgram;
+
+	int w = df::Backbuffer.getWidth(), h = df::Backbuffer.getHeight();
+	auto frameBuff = df::Renderbuffer<df::depth24>(w, h) + df::Texture2D<>(w, h, 1);
+
+	sam.AddResize([&](int w, int h) {frameBuff = frameBuff.MakeResized(w, h); });
+	
+	GL_CHECK; //extra opengl error checking in GPU Debug build configuration
+
+	sam.Run([&](float deltaTime) //delta time in ms
+		{
+			cam.Update();
+
+			frameBuff << df::Clear() << program << "texImg" << testTex;
+			program << demoVao;	//Rendering: Ensures that both the vao and program is attached
+
+			df::Backbuffer << df::Clear() << postprocess << "texFrame" << frameBuff.get<glm::u8vec3>();
+			postprocess << df::NoVao(GL_TRIANGLES, 3); // Rendering a pixel shader
+
+			GL_CHECK;
+			program.Render(); postprocess.Render(); //only the UI!!
+		}
+	);
+	return 0;
+}
