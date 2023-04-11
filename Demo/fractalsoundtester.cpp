8def2f4d01e22a2d87645b80bd8e70989bcf2a38
Líviusz Szalma <anonlius@gmail.com>
2023-04-11 20:05:38 +0200

    Create testing macros

--- Demo/fractalsoundtester.cpp
+++ Demo/fractalsoundtester.cpp
@@ -2,6 +2,45 @@
 #include <iostream>
 
 void FractalSoundTester::FractalSoundConstructor()
+#define ASSERT_EQ(actual, expected)                                 \
+    if(actual != expected){                                         \
+        std::cout << "Assert failed while running " << __func__     \
+                  << ", at line " << __LINE__ << std::endl          \
+                  << "Expected: " << expected                       \
+                  << "(" << #expected << ")" << std::endl           \
+                  << "Actual: " << actual                           \
+                  << "(" << #actual << ")" << std::endl;            \
+        return false;                                               \
+    }
+
+#define ASSERT_NE(actual, expected)                                 \
+    if(actual == expected){                                         \
+        std::cout << "Assert failed while running " << __func__     \
+                  << ", at line " << __LINE__ << std::endl          \
+                  << "Expected: " << expected                       \
+                  << "(" << #expected << ")" << std::endl           \
+                  << "Actual: " << actual                           \
+                  << "(" << #actual << ")" << std::endl;            \
+        return false;                                               \
+    }
+
+#define ASSERT_TRUE(arg)                                            \
+    if(!(arg)) {                                                    \
+        std::cout << "Assert failed while running " << __func__     \
+                  << ", at line " << __LINE__ << std::endl          \
+                  << "Expected " << arg << "(" << #arg << ")"       \
+                  << " to be true, but it was false" << std::endl;  \
+        return false;                                               \
+    }
+
+#define ASSERT_FALSE(arg)                                           \
+    if(arg) {                                                       \
+        std::cout << "Assert failed while running " << __func__     \
+                  << ", at line " << __LINE__ << std::endl          \
+                  << "Expected " << arg << "(" << #arg << ")"       \
+                  << " to be false, but it was true" << std::endl;  \
+        return false;                                               \
+    }
 {
     int FS = 10;
     FractalSound* fractalSound = new FractalSound(FS);
