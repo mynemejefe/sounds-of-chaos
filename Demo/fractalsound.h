3a7a1acec35264cf43a39d11f553f1615a8b95d0
Líviusz Szalma <anonlius@gmail.com>
2023-05-28 13:29:13 +0200

    Modify visibility of members

--- Demo/fractalsound.h
+++ Demo/fractalsound.h
@@ -18,21 +18,20 @@ public:
 	FractalSound(int fs);
 	~FractalSound();
 	void PlaySoundAtPos(InputVars inputVars, FractalVars fractalVars, SoundVars soundVars);
-	float* CreateSoundBufferFromLastPos(InputVars inputVars, FractalVars fractalVars, SoundVars soundVars);
 	bool UsePianoKey(InputVars inputVars, FractalVars fractalVars, SoundVars soundVars, int n);
-	void PlaySoundFromMixChunk(Mix_Chunk* chunkToPlay);
 
 	inline const int GetFs() { return fs_; };
 private:
 	int fs_;
 	PianoKey pianoKeys[10]{};
+	friend class FractalSoundTester;
 
 	Mix_Chunk* CreateMixChunk(int volume);
 	void Mix_FreeChunk(Mix_Chunk* chunk);
-	std::vector<float> ApplyKernelToDistances(std::vector<float> distances, std::vector<int> kernel);
 	bool FillBufferAM(InputVars inputVars, FractalVars fractalVars, SoundVars soundVars, float buff[]);
 	bool FillBufferAdditive(InputVars inputVars, FractalVars fractalVars, SoundVars soundVars, float buff[]);
-
-	friend class FractalSoundTester;
+	float* CreateSoundBufferFromLastPos(InputVars inputVars, FractalVars fractalVars, SoundVars soundVars);
+	void PlaySoundFromMixChunk(Mix_Chunk* chunkToPlay);
+	std::vector<float> ApplyKernelToDistances(std::vector<float> distances, std::vector<int> kernel);
 };
 
