f7c6ecdef2ac91329bad086852c00f9766b6a5f1
Líviusz Szalma <anonlius@gmail.com>
2023-05-28 11:02:16 +0200

    Rename project and shader

--- Demo/main.cpp
+++ Demo/main.cpp
@@ -8,6 +8,11 @@
 
 int main(int argc, char* args[])
 {
+#ifdef TESTING
+	FractalSoundTester* tester = new FractalSoundTester();
+	tester->RunAllTests();
+#else
+
 	df::Sample sam("Sounds of chaos", 800, 800, df::Sample::FLAGS::DEFAULT);
 	df::Camera cam;								// Implements a camera event class with handles
 	cam.SetView(glm::vec3(0, 0, 0), glm::vec3(0, 1, 0), glm::vec3(0, 0, 1));
@@ -18,10 +23,10 @@ int main(int argc, char* args[])
 	eltecg::ogl::ArrayBuffer MyVBO;	MyVBO.constructMutable(std::vector<glm::vec2>{ {-1, -1}, { 1, -1 }, { -1, 1 }, { 1, 1 }}, GL_STATIC_DRAW);
 	eltecg::ogl::VertexArray MyVAO;	MyVAO.addVBO<glm::vec2>(MyVBO);
 
-	df::VaoArrays demoVao((GLuint)MyVAO, GL_TRIANGLE_STRIP, 4, 0u);
+	df::VaoArrays vao((GLuint)MyVAO, GL_TRIANGLE_STRIP, 4, 0u);
 
-	df::ShaderProgramVF program = "MyShaderProgram";
-	program << "Shaders/vert.vert"_vert << "Shaders/frag.frag"_frag << df::LinkProgram;
+	df::ShaderProgramVF fractalShader = "FractalShaderProgram";
+	fractalShader << "Shaders/vert.vert"_vert << "Shaders/frag.frag"_frag << df::LinkProgram;
 	df::ShaderProgramVF postprocess = "Postprocess shader program";
 	postprocess << "Shaders/postprocess.vert"_vert << "Shaders/postprocess.frag"_frag << df::LinkProgram;
 
@@ -38,11 +43,6 @@ int main(int argc, char* args[])
 	GraphicVars graphicVars;
 
 	FractalSound* fractalSound = new FractalSound(FS);
-
-#ifdef TESTING
-	FractalSoundTester* tester = new FractalSoundTester();
-	tester->RunAllTests();
-#else
 
 	sam.AddMouseMotion([&](SDL_MouseMotionEvent e) { return true; }, 6);
 	sam.AddMouseWheel([&](SDL_MouseWheelEvent wheel)
@@ -139,7 +139,7 @@ int main(int argc, char* args[])
 			}
 			ImGui::End();
 
-			frameBuff << df::Clear() << program
+			frameBuff << df::Clear() << fractalShader
 				<< "offset" << glm::vec2(cam.GetEye().x, cam.GetEye().y)
 				<< "last_click" << inputVars.lastClickPos
 				<< "last_click_col" << glm::vec3(graphicVars.lastClickColor[0], graphicVars.lastClickColor[1], graphicVars.lastClickColor[2])
@@ -150,13 +150,13 @@ int main(int argc, char* args[])
 				<< "max_iter" << fractalVars.maxIterations
 				<< "fractal_type" << fractalVars.fractalType
 				<< "power" << fractalVars.power;
-			program << demoVao;	//Rendering: Ensures that both the vao and program is attached
+			fractalShader << vao;	//Rendering: Ensures that both the vao and program is attached
 
 			df::Backbuffer << df::Clear() << postprocess << "texFrame" << frameBuff.get<glm::u8vec3>();
 			postprocess << df::NoVao(GL_TRIANGLES, 3); // Rendering a pixel shader
 
 			GL_CHECK;
-			program.Render(); postprocess.Render(); //only the UI!!
+			fractalShader.Render(); postprocess.Render(); //only the UI!!
 		}
 	);
 
